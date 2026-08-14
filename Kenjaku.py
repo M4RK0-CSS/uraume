a4c56b10fe905f52b49e91f653630602d575fda2
Lumin3 <dcht1954@gmail.com>
2026-08-14 09:47:46 -0300

    Kenjaku.py

--- Kenjaku.py
+++ Kenjaku.py
@@ -1,6 +1,6 @@
-print("hello master")
-senha = "DeadBody"
-if senha != "DeadBody"
-print ("welcome kenjaku-sama")
-else 
-print ("You are not Him, go away...")
+print("hello User")
+senha = input("who are you?")
+if senha != "DeadBody":
+ print ("welcome kenjaku-sama") 
+else:
+ print ("You are not Him, go away...")
