44c005139f9cbdecf10f6ac14d717db6289dfaa4
Lumin3 <dcht1954@gmail.com>
2026-08-14 10:51:36 -0300

    Kenjaku.py

--- Kenjaku.py
+++ Kenjaku.py
@@ -1,6 +1,27 @@
 print("==𝙳𝙴𝙰𝙳 𝙱𝙾𝙳𝚈==")
-senha = input("who are you?")
+
+senha = input("Who are you? ")
+
 if senha == "Kenjaku":
- print ("welcome kenjaku-sama") 
+    print("Welcome Kenjaku-sama!")
+
+    # Login validado
+    while True:
+        print("\n=== Kenjaku's Brain ===")
+        print("[1] Kenjaku's brute force")
+        print("[0] Exit")
+
+        opcao = input("Choose: ")
+
+        if opcao == "1":
+            print("Kenjaku's brute force selected")
+
+        elif opcao == "0":
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid option!")
+
 else:
- print ("You are not Him, go away...")
+    print("You are not Him, go away...")
